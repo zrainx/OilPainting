8300af88d267825ed2d346da7bd2a7453d428b14
zrainx <zrainx@foxmail.com>
2016-12-04 19:19:53 +0800

    fix some bug

--- paint.m
+++ paint.m
@@ -5,13 +5,14 @@ function [ canvas ] = paint( sourceImage,paintParameters )
     fc=paintParameters.fc;
     for Ri = R       
         G=(2*Ri+1)*fc;
-        m=fspecial('gaussian',G);
+        m=fspecial('gaussian',G,G);
         referenceImage = imfilter(sourceImage,m,'replicate');
-        m=fspecial('sobel');
-        grayReferenceImage=rgb2gray(referenceImage);
-        gx=imfilter(grayReferenceImage,m,'replicate');
-        m=m';
-        gy=imfilter(grayReferenceImage,m,'replicate');
+%         m=fspecial('sobel');
+%         grayReferenceImage=rgb2gray(referenceImage);
+%         gx=imfilter(grayReferenceImage,m,'replicate');
+%         m=m';
+%         gy=imfilter(grayReferenceImage,m,'replicate');
+        [gx,gy]=gradient(referenceImage);
         canvas = paintLayer(canvas,referenceImage,gx,gy,Ri,paintParameters);
         
         subplot(1,2,2);

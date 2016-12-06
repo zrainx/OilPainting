0d52b3f021dc38c5cc18f45eb0cf1affc9f40d2f
zrainx <zrainx@foxmail.com>
2016-12-07 00:10:39 +0800

    Update makeStroke.m

--- makeStroke.m
+++ makeStroke.m
@@ -8,20 +8,20 @@ maxStrokeLength=paintParameters.maxLength;
 minStrokeLength=paintParameters.minLength;
 fc=paintParameters.fc;
 for i=1:maxStrokeLength
-   
+    
     if (i>minStrokeLength) && ...
-       norm(reshape(referenceImage(r,c,:)-canvas(r,c,:),3,[]))< ...
-       norm(reshape(referenceImage(r,c,:)-strokeColor,3,[]))
-       return;
+            norm(reshape(referenceImage(r,c,:)-canvas(r,c,:),3,[]))< ...
+            norm(reshape(referenceImage(r,c,:)-strokeColor,3,[]))
+        return;
     end
-
+    
     if norm([gx(r,c),gy(r,c)])==0
         return;
     end
     dx=-gy(r,c); dy=gx(r,c);
-     if (lastDx * dx + lastDy * dy)<0
-         dx=-dx; dy=-dy;
-     end
+    if (lastDx * dx + lastDy * dy)<0
+        dx=-dx; dy=-dy;
+    end
     dx=fc*dx+(1-fc)*lastDx;
     dy=fc*dy+(1-fc)*lastDy;
     dx=dx/sqrt(dx^2+dy^2);
@@ -30,7 +30,7 @@ for i=1:maxStrokeLength
     r=min(r,imageHeight);c=min(c,imageWidth);
     r=max(r,1);c=max(c,1);
     lastDx=dx;lastDy=dy;
-    K=[K [r;c]];   
+    K=[K [r;c]];
 end
 
 return;

3d7aa0910d8a1a3834b1cac6e394fe23952b1611
zrainx <zrainx@foxmail.com>
2016-12-06 20:33:57 +0800

    Update makeStroke.m

--- makeStroke.m
+++ makeStroke.m
@@ -3,7 +3,7 @@ function K=makeStroke(R,r,c,referenceImage,gx,gy,canvas,paintParameters)
 strokeColor=referenceImage(r,c,:);
 K=[]; K=[K [r;c]];
 lastDx=0; lastDy=0;
-[imageHeight,imageWidth,channel]=size(referenceImage);
+[imageHeight,imageWidth,~]=size(referenceImage);
 maxStrokeLength=paintParameters.maxLength;
 minStrokeLength=paintParameters.minLength;
 fc=paintParameters.fc;
@@ -15,28 +15,22 @@ for i=1:maxStrokeLength
        return;
     end
 
-%     if (i>minStrokeLength) && ...
-%        sum(reshape(referenceImage(x,y,:)-canvas(x,y,:),3,[])< ...
-%        reshape(referenceImage(x,y,:)-strokeColor,3,[]))
-%        return;
-%     end
-
     if norm([gx(r,c),gy(r,c)])==0
         return;
     end
-    dx=gy(r,c); dy=-gx(r,c);
-    if (lastDx * dx + lastDy * dy)<0
-        dx=-dx; dy=-dy;
-    end
+    dx=-gy(r,c); dy=gx(r,c);
+     if (lastDx * dx + lastDy * dy)<0
+         dx=-dx; dy=-dy;
+     end
     dx=fc*dx+(1-fc)*lastDx;
     dy=fc*dy+(1-fc)*lastDy;
     dx=dx/sqrt(dx^2+dy^2);
     dy=dy/sqrt(dx^2+dy^2);
-    r=r+floor((2*R+1)*dx);c=c+floor((2*R+1)*dy);
+    c=c+floor((2*R+1)*dx);r=r+floor((2*R+1)*dy);
     r=min(r,imageHeight);c=min(c,imageWidth);
     r=max(r,1);c=max(c,1);
-    K=[K [r;c]];
     lastDx=dx;lastDy=dy;
+    K=[K [r;c]];   
 end
 
 return;
